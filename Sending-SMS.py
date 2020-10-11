5a137c3ee3ec7f01f43489bb45203f8d0a82674c
akhilaku <akhildasks1999@gmail.com>
2020-10-11 19:02:29 +0530

    modified the code

--- Sending-SMS.py
+++ Sending-SMS.py
@@ -5,12 +5,16 @@ from twilio.rest import Client
 client = Client("ACxxxxxxxxxxxxxx", "zzzzzzzzzzzzz")
 
 # this is the URL to an image file we're going to send in the MMS
-media = "https://raw.githubusercontent.com/mattmakai/fullstackpython.com/master/static/img/logos/f.png"
+media = "https://Your_Media_image's_Link"
 
 # change the "from_" number to your Twilio number and the "to" number
 # to the phone number you signed up for Twilio with, or upgrade your
 # account to send MMS to any phone number that MMS is available
-client.api.account.messages.create(to="+19732644152",
-                                   from_="+12023351278",
-                                   body="MMS via Python? Nice!",
+client.api.account.messages.create(to="Your_Phone_Number",
+                                   from_="Your_Twilio_Number",
+                                   media_url=media
+                                   body="Human Presence detected",
+                                   "Location:"
+                                   "Latitude:"= media(maps.lat()).found()"
+                                   "Longitude:"=media(maps.long()).found()"
                                    media_url=media)
